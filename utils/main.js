da7345547e8cd486070849ca4b174e0468bdfc79
iren <irasibil2@gmail.com>
2023-10-25 12:33:18 +0300

    make carousel in verticals section work

--- utils/main.js
+++ utils/main.js
@@ -1,3 +1,4 @@
+// menu open-close
 const iconMenu = document.querySelector('.header__nav-menu-icon');
 const menuBody = document.querySelector('.header__nav-items');
 if (iconMenu) {
@@ -7,3 +8,31 @@ if (iconMenu) {
         menuBody.classList.toggle('_active');
     });
 }
+
+
+// carousel
+const btnPrev = document.querySelector('.btn-prev');
+const btnNext = document.querySelector('.btn-next');
+const gallery = document.querySelector('.verticals__carousel-gallery');
+const galleryList = document.querySelector('.verticals__carousel-items');
+const scrollWidth = 310;
+
+btnPrev.addEventListener("click", function () {
+    gallery.scrollBy({
+        left: -scrollWidth,
+        behavior: 'smooth'
+    });
+
+    btnNext.classList.remove("no-active");
+    btnPrev.classList.toggle("no-active", gallery.scrollLeft <= scrollWidth);
+});
+
+btnNext.addEventListener("click", function () {
+    gallery.scrollBy({
+        left: scrollWidth,
+        behavior: 'smooth'
+    });
+
+    btnPrev.classList.remove("no-active");
+    btnNext.classList.toggle("no-active", galleryList.clientWidth - (gallery.scrollLeft + scrollWidth) < gallery.clientWidth);
+});
